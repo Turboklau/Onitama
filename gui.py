6b582775fe0b62520999491960e095595851c258
Jacob Rogers <jacob.p.r@hotmail.com>
2019-10-29 22:44:07 +1300

    added GUI move images

--- gui.py
+++ gui.py
@@ -6,16 +6,15 @@ from PIL import ImageTk, Image
 
 class OnitamaGUI:
     def __init__(self):
+        self.images = {}
+        self.actions = [0,0,0,0,0]
         self.root = tk.Tk()
         self.game = Game()
         self.game.set_up()
 
-        self.actions = tk.Frame(self.root)
-        self.actions.grid(row=2,column=0)
-        self.board = tk.Frame(self.root)
-        self.board.grid(row=1,column=1)        
+      
         self.create_board()
-        self.update_actions()
+        self.create_actions()
 
         self.count = 0
 
@@ -37,17 +36,17 @@ class OnitamaGUI:
             self.reset()
 
         self.update_board()
+        self.update_actions()
 
     def reset(self):
         self.game.reset()
-        print(self.game.board_state)
 
-    def pvp(self):
-        self.pvp.destroy()
-
-
+    def terminate(self):
+        self.root.destroy()
 
     def create_board(self):
+        self.board = tk.Frame(self.root)
+        self.board.grid(row=1,column=1)  
         #board = [ [None]*5 for _ in range(5) ]
         self.labelgrid = [ [None]*5 for _ in range(5) ]
         for i,row in enumerate(self.game.board_state):
@@ -60,7 +59,6 @@ class OnitamaGUI:
                 L.grid(row=i,column=j)
                 L.bind('<Button-1>',lambda e,i=i,j=j: self.on_click(i,j,e))
                 self.labelgrid[i][j] = L
-                #tk.ttk.Separator(self.root, orient='vertical').grid(column=1, row=0, rowspan=5, sticky='ns')
 
     def update_board(self):
         for i,row in enumerate(self.game.board_state):
@@ -70,53 +68,55 @@ class OnitamaGUI:
 
                 self.labelgrid[i][j]['text'] = text
                 self.labelgrid[i][j]['bg'] = colour
-    def update_actions(self):
+    
+    def load_image(self, card):
+        img = Image.open("res/" + card +".png")
+        img = img.resize((200,200), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        self.images[card] = img
 
+
+    def create_actions(self):
         self.left = tk.Frame(self.root)
         self.left.grid(row=1, column=0)
-
         self.right = tk.Frame(self.root)
         self.right.grid(row=1, column=2)
-
         self.extra = tk.Frame(self.root)
         self.extra.grid(row=2, column=1)
-
-        P1 = tk.Label(self.left,text="Player 1 Cards")
+        P1 = tk.Label(self.left,text="Red Player Cards")
         Ex = tk.Label(self.extra,text="Extra")
-        P2 = tk.Label(self.right,text="Player 2 Cards")
-
-        #P1.pack(side="left", anchor="w", fill='x')
-        #P2.pack(side="right", anchor="e", fill='x')
-        P1.grid(row=0,column=0)#, sticky='s')
+        P2 = tk.Label(self.right,text="Blue Player Cards")
+        P1.grid(row=0,column=0)
         Ex.grid(row=0,column=0)
-        P2.grid(row=0,column=0)#, sticky='s')
+        P2.grid(row=0,column=0)
 
-        inc = [0,0,0]
-        count = 1
+        y = [0,1, 0, 0,1]
+        x = [self.left, self.left, self.extra, self.right, self.right]
+        for i in range(5):
+            panel = tk.Label(x[i], image = None, text="h")
+            panel.grid(row=y[i]+1,column=0)
+            self.actions[i] = panel
+        self.update_actions()
+
+    def update_actions(self):
+        inc = [0,2,3]
+
         for card in self.game.cards:
-            img = ImageTk.PhotoImage(Image.open(str(count)+".jpg"))
-            count+= 1 
+            if card.name not in self.images.keys():
+                self.load_image(card.name)
 
             if card.holder == "red":
                 row = 0
-                target=self.right
             elif card.holder == "blue":
-                row = 1
-                target=self.left
-            else:
                 row = 2
-                target=self.extra
+            else:
+                row = 1
 
+            self.actions[inc[row]]["image"] = self.images[card.name]
             inc[row] += 1
-            panel = tk.Label(target, image = img)
-            panel.photo = img
-            panel.grid(row=inc[row],column=0)
 
 
-        #panel = tk.Label(self.actions, image = img)
-        #panel.photo = img
 
-        #panel.grid(row=1,column=0)
 
 def robot_turn(robot, game):
     move = robot.decide_move(game)
@@ -129,6 +129,7 @@ def robot_turn(robot, game):
                 break
 
     game.end_turn()
+    return move.card.name
 
 x = OnitamaGUI()
 
