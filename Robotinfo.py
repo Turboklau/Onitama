02b0ff40ef49f3f906073de0a7f63bb8c97dbe3c
Torben <tkl34@uclive.ac.nz>
2019-11-04 13:09:38 +1300

    Made some small changes

--- Robotinfo.py
+++ Robotinfo.py
@@ -1,26 +1,26 @@
 import copy
 
 class Robotinfo():
-	def __init__(game):
+	def __init__(self, game):
 		self.game = game
 
-	def get_board_class():
-		return copy.deep_copy(self.game.board)
+	def get_board_class(self):
+		return copy.deepcopy(self.game.board)
 
-	def get_board():
-		return copy.deep_copy(self.game.board.board_state)
+	def get_board(self):
+		return copy.deepcopy(self.game.board.board_state)
 
-	def get_hand(player):
+	def get_hand(self, player):
 		return self.game.players[player].hand
 
-	def get_midcard():
+	def get_midcardself(self):
 		return self.game.mid_card
 
-	def get_cards():
+	def get_cards(self):
 		cards = []
 		for player in self.game.players:
 			cards += player.hand
 		return cards + self.get_midcard()
 
-	def in_hand(card, player):
+	def in_hand(self, card, player):
 		return card in self.get_hand(player)
